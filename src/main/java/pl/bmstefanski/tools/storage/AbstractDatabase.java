091bc460fefb0023993a7528312c46efe9faa05f
Bartłomiej Stefański <bstefanskicom@gmail.com>
2018-01-21 12:19:35 +0100

    License

--- src/main/java/pl/bmstefanski/tools/storage/AbstractDatabase.java
+++ src/main/java/pl/bmstefanski/tools/storage/AbstractDatabase.java
@@ -1,3 +1,27 @@
+/*
+ MIT License
+
+ Copyright (c) 2018 Whippy Tools
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 package pl.bmstefanski.tools.storage;
 
 import pl.bmstefanski.tools.api.storage.Database;
@@ -7,6 +31,11 @@ import pl.bmstefanski.tools.type.StatementType;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
+
+ /*
+    Author: MarcinWieczorek
+    Source: https://github.com/MarcinWieczorek/NovaGuilds/blob/master/src/main/java/co/marcin/novaguilds/impl/storage/AbstractDatabaseStorage.java
+ */
 
 public abstract class AbstractDatabase implements Database, Storage {
 

45ac63aac427ba775fdfbe67f7376c85a7b1d8b6
Jatti <wolereq@gmail.com>
2018-01-23 19:07:09 +0100

    Add Skull Command

--- src/main/java/pl/bmstefanski/tools/command/SkullCommand.java
+++ src/main/java/pl/bmstefanski/tools/command/SkullCommand.java
@@ -14,12 +14,12 @@ import pl.bmstefanski.tools.command.basic.CommandInfo;
 import pl.bmstefanski.tools.storage.configuration.Messages;
 import pl.bmstefanski.tools.util.MessageUtils;
 
-public class SkullCommand implements MessageUtils{
+public class SkullCommand implements MessageUtils {
 
     private final Tools plugin;
     private final Messages messages;
 
-    public SkullCommand(Tools plugin){
+    public SkullCommand(Tools plugin) {
         this.plugin = plugin;
         this.messages = plugin.getMessages();
     }
@@ -31,13 +31,13 @@ public class SkullCommand implements MessageUtils{
             userOnly = true,
             usage = "[player]"
     )
-    public void skull(CommandSender sender, CommandContext context){
-
+    public void skull(CommandSender sender, CommandContext context) {
+        
         Player player = (Player) sender;
         ItemStack skullItem = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
         SkullMeta skullMeta = (SkullMeta) skullItem.getItemMeta();
 
-        if(context.getArgs().length == 0){
+        if (context.getArgs().length == 0) {
             skullMeta.setOwningPlayer(player);
             skullItem.setItemMeta(skullMeta);
             player.getInventory().addItem(skullItem);
